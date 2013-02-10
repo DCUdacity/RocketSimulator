e00db85a1e4237d05fe1d125c2a1f5c6ef0c5543
Sarkar <asarka001c@HQSML-142535.(none)>
2013-02-10 16:52:18 -0500

    linked the main.html file to rocketsimulator.py

--- rocketsimulator.py
+++ rocketsimulator.py
@@ -3,7 +3,7 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 class MainPage(webapp.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello from Rrocket Simulator!')
+        self.response.out.write('html/main.html')
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage)],
