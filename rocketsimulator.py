4a7022150fc8680b9a9b96c76da646d6e7b99951
Sarkar <asarka001c@HQSML-142535.(none)>
2013-02-10 16:50:06 -0500

    changed the app structure for now

--- rocketsimulator.py
+++ rocketsimulator.py
@@ -1,6 +1,5 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-import Welcome
 class MainPage(webapp.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
